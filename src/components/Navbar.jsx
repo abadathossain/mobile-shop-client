bc43aba5c43b663d4372a4aed2726d57f69bfa21
Abadat Hossain <abadat26@gmail.com>
2024-12-20 16:05:20 +0600

    home page and login info done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
 
     </>
     return (
-        <div className="navbar bg-base-300">
+        <div className="navbar bg-sky-200">
             <div className="navbar-start">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
